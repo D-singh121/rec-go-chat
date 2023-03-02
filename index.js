8581411d2820c2c9fedb21ffa1d508766e29bb9d
Devesh Choudhary <108144751+D-singh121@users.noreply.github.com>
2023-03-02 14:05:33 +0530

    main file

--- index.js
+++ index.js
@@ -1,32 +1,17 @@
-var socket = new WebSocket("ws://localhost:8080/ws");
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-//-----after chathistory creation we pass a callback parameter "cb"
-let connect = (cb) => {
-    console.log("Attempting Connection...");
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-    socket.onopen = () => {
-        console.log("Successfully Connected");
-    }
-
-    socket.onmessage = msg => {
-        console.log(msg);
-        cb(msg)// callback for history messages . 
-    };
-
-    socket.onclose = event => {
-        console.log("Socket Closed Connection: ", event);
-    };
-
-    socket.onerror = error => {
-        console.log("Socket Error: ", error);
-    };
-}
-
-
-
-let sendMsg = msg =>{
-    console.log("sending messages ", msg);
-    socket.send(msg);
-};
-
-export {connect ,sendMsg};
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
